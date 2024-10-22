d661a75cd69dc5e618d1fdecbab75883550a6c52
Nguyễn Ngọc Phúc <ngoc_phuc@gonosen.asia>
2024-10-22 11:25:43 +0700

    login page

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -3,10 +3,21 @@ import React from 'react';
 const Login = () => {
   return (
     <>
-      <div className="login-container">
-        login
-        <button>test</button>
-      </div>
+      <section className="main-body content-login">
+        <div className="login">
+          <div className="logo">
+            <a href="#">VIDEO AI</a>
+          </div>
+          <div className="title">ログイン</div>
+          <form>
+            <input type="text" placeholder="ID" />
+            <input type="password" placeholder="パスワード" />
+            <button className="button btn-gradient" type="button">
+              ログイン
+            </button>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
